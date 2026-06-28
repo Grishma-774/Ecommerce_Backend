ea47ae12bc969e14e24890183fc58ee9377591ca
Grishma-774 <gellaboinagrishma@gmail.com>
2026-06-28 19:04:08 +0530

    added_allowed_host_new

--- Backend_Ecommerce/ecommerce_backend/ecommerce_backend/settings.py
+++ Backend_Ecommerce/ecommerce_backend/ecommerce_backend/settings.py
@@ -154,6 +154,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS =[
+    "http://localhost:5173",
     "https://ecommerce-website-mu-henna-97.vercel.app"
 ]
 
