0238bad78a8aa3642af1ee9ae108a1f1a83b9d2f
Grishma-774 <gellaboinagrishma@gmail.com>
2026-06-30 13:26:36 +0530

    Specify Python 3.12.10 for deployment

--- Backend_Ecommerce/ecommerce_backend/ecommerce_backend/settings.py
+++ Backend_Ecommerce/ecommerce_backend/ecommerce_backend/settings.py
@@ -154,7 +154,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS =[
-    "http://localhost:5173",
     "https://ecommerce-website-mu-henna-97.vercel.app"
 ]
 
